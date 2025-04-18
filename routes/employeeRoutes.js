8b9aca3d1832353c7a21384bd7a522ffec1b633a
adam-dev2 <shaikadam252@gmail.com>
2025-04-18 16:40:05 +0530

    just added the attendance feature

--- routes/employeeRoutes.js
+++ routes/employeeRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const Authentication = require('../middleware/Authentication');
-const { createEmployee, editEmployee, deleteEmployee } = require('../controllers/employeeController');
+const { createEmployee, editEmployee, deleteEmployee, markAttendance } = require('../controllers/employeeController');
 const router = express.Router();
 
 router.post('/create',Authentication,createEmployee);
 router.put('/edit/:id',Authentication,editEmployee);
 router.delete('/delete/:id',Authentication,deleteEmployee);
+
+router.post('/attendance',Authentication,markAttendance);
 
 module.exports = router;
